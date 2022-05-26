f24470a8cc742aa547e0d650ca500297aacc73e6
David <chobanyandav@gmail.com>
2022-05-26 19:05:20 +0300

    userData commit

--- client/src/redux/reducers/profileReducer/profileReducer.js
+++ client/src/redux/reducers/profileReducer/profileReducer.js
@@ -1,7 +1,7 @@
 import {initState} from "../../initState/initState";
 import {SET_PROFILE} from "../../types/types";
 
-export default function profileReducer(state = initState().user, action) {
+export default function profileReducer(state = initState().userData, action) {
     switch (action.type) {
         case SET_PROFILE:
             return action.payload;
