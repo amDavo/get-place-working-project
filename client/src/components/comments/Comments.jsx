c2914fdae44dc94c03462f44855e27db18fb4ebc
David <chobanyandav@gmail.com>
2022-05-31 10:31:56 +0300

    work1

--- client/src/components/comments/Comments.jsx
+++ client/src/components/comments/Comments.jsx
@@ -1,27 +1,27 @@
-import React, { useEffect, useState } from 'react';
+import React, {useEffect, useState} from 'react';
 
 function Comments({id}) {
-  const [comments, setComments] = useState([])
-  const [inputs, setInputs] = useState('')
-  useEffect(() => {
-    fetch(`http://localhost:8080/comments/${id}`).then(res => res.json()).then(data => setComments(data))
-  }, [])
-  const addHandler = async() => {
-    const response = await fetch(`http://localhost:8080/comments/${id}`, {
-      method: 'post',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({inputs})
-    })
-  }
-  console.log(comments);
-  return (
-    <div className='commentForm'>
-    <input onChange={(e) => setInputs(e.target.value)}></input>
-    <button onClick={addHandler}>Добавить </button>
-    </div>
-  );
+    const [comments, setComments] = useState([])
+    const [inputs, setInputs] = useState('')
+    useEffect(() => {
+        fetch(`http://localhost:8080/comments/${id}`).then(res => res.json()).then(data => setComments(data))
+    }, [])
+    const addHandler = async () => {
+        const response = await fetch(`http://localhost:8080/comments/${id}`, {
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({inputs})
+        })
+    }
+    console.log(inputs);
+    return (
+        <div className='commentForm'>
+            <input onChange={(e) => setInputs(e.target.value)}/>
+            <button onClick={addHandler}>Добавить</button>
+        </div>
+    );
 }
 
 export default Comments;
