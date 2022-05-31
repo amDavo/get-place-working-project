ced0114f4c4996832ebd6e09ba22bcd8d947051d
David <chobanyandav@gmail.com>
2022-05-31 10:23:04 +0300

    work

--- client/src/components/UI/addToFavButton/FavButton.jsx
+++ client/src/components/UI/addToFavButton/FavButton.jsx
@@ -1,18 +1,15 @@
 import React, {useState} from 'react';
-import {useSelector,useDispatch} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {addFavorite} from "../../../redux/thunk/favoritesThunks/addFavorite";
 import './module.css'
-import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 
 const FavButton = ({cardData}) => {
     const [show, setShow] = useState(false)
-    const info = useSelector(state=>state.infoFavoriteIsSuccess)
+    const info = useSelector(state => state.infoFavoriteIsSuccess)
     const dispatch = useDispatch();
 
-
     const addToFavHandler = () => {
-
-        if( cardData.id){
+        if (cardData.id) {
             dispatch(addFavorite(cardData.id))
             setShow(true)
         }
@@ -20,7 +17,7 @@ const FavButton = ({cardData}) => {
 
     return (
         <>
-            <img alt='favorite'  className='fav-btn' src="https://img.icons8.com/stickers/100/000000/heart-plus.png"/>
+            <img alt='favorite' className='fav-btn' src="https://img.icons8.com/stickers/100/000000/heart-plus.png"/>
         </>
     );
 };
