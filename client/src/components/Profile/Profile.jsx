5192b6804171c0ae002f78c7a906b2413062de45
David <chobanyandav@gmail.com>
2022-06-02 10:42:22 +0300

    merge commit

--- client/src/components/Profile/Profile.jsx
+++ client/src/components/Profile/Profile.jsx
@@ -3,11 +3,9 @@ import {useEffect, useState} from "react";
 import {setUserInfo} from "../../redux/thunk/profileThunk/profileThunk";
 import {signChange} from "../../redux/actions/userAction";
 import {useNavigate} from "react-router-dom";
-import PlaceCardSmall from "../placeCardSmall/PlaceCardSmall";
-import {setInfoFavoriteIsSuccess} from "../../redux/actions/favoriteAction/favorite.action";
-import {getFavorite} from "../../redux/thunk/favoritesThunks/addFavorite";
 import classes from './Profile.module.css'
 import ProfileModal from './ProfileModal'
+import PlaceCardSmall from "../placeCardSmall/PlaceCardSmall";
 
 function Profile() {
     const dispatch = useDispatch();
@@ -15,6 +13,7 @@ function Profile() {
     const userId = useSelector(state => state.user)
     const favorites = useSelector(state => state.userFavorites)
     const [show, setShow] = useState(false)
+    const [viewModal, setViewModal] = useState(false)
     const [userChange, setUserChange] = useState({
         name: '',
         email: '',
@@ -22,9 +21,7 @@ function Profile() {
         nickname: ''
     });
 
-    console.log(user, '======w=w=ww=w=w=')
-
-    const handleClose=()=>{
+    const handleClose = () => {
         setViewModal(false)
     }
     const navigate = useNavigate();
@@ -45,21 +42,24 @@ function Profile() {
         }
     };
     return (<>
-            <div className = {classes.Profile}>
-                <div className = {classes.Data}>Данные профиля</div>
-                <div className = {classes.Name}>Имя: <input type = "text" value={user.name}/></div>
-                <div className = {classes.Login}>Логин: <input type = "text" value={user.nickname}/></div>
-                <div className={classes.Email}>E-mail: <input type = "text" value={user.email}/></div>
+            <div className={classes.Profile}>
+                <div className={classes.Data}>Данные профиля</div>
+                <div className={classes.Name}>Имя: <input type="text" value={user.name}/></div>
+                <div className={classes.Login}>Логин: <input type="text" value={user.nickname}/></div>
+                <div className={classes.Email}>E-mail: <input type="text" value={user.email}/></div>
             </div>
-            <button className = {classes.BtnProf} onClick={() => {
+            <button className={classes.BtnProf} onClick={() => {
                 setShow(prev => !prev)
-                setViewModal(prev=> !prev)
+                setViewModal(prev => !prev)
             }}>
                 {!show ? 'Редактировать' : 'Отменить'}
             </button>
             {show && (
                 <ProfileModal open={viewModal} close={handleClose} user={user}/>
             )
+            }
+            {
+                favorites?.length && favorites?.map(el => <PlaceCardSmall view={true} cardData={el} key={el.id}/>)
             }
         </>
     )
